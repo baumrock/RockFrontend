3f4d78f52d99367f608b9c71e796acb58d9f35dd
BernhardBaumrock <office@baumrock.com>
2022-03-21 15:33:52 +0100

    tweak indentation

--- ScriptsArray.php
+++ ScriptsArray.php
@@ -1,11 +1,12 @@
 <?php namespace RockFrontend;
 class ScriptsArray extends AssetsArray {
 
-  public function render() {
+  public function render($indent = '') {
     $out = '';
     foreach($this as $script) {
       $m = $script->m ? "?m=".$script->m : "";
-      $out .= "  <script src='{$script->url}$m'{$script->suffix}></script>\n";
+      $out .= "$indent<script src='{$script->url}$m'{$script->suffix}></script>\n";
+      $indent = '  ';
     }
     return $out;
   }
--- StylesArray.php
+++ StylesArray.php
@@ -1,11 +1,12 @@
 <?php namespace RockFrontend;
 class StylesArray extends AssetsArray {
 
-  public function render() {
+  public function render($indent = '') {
     $out = '';
     foreach($this as $asset) {
       $m = $asset->m ? "?m=".$asset->m : "";
-      $out .= "  <link rel='stylesheet' href='{$asset->url}$m'{$asset->suffix}>\n";
+      $out .= "$indent<link rel='stylesheet' href='{$asset->url}$m'{$asset->suffix}>\n";
+      $indent = '  ';
     }
     return $out;
   }
