f55d604baccdfe7ee0d0f5d60c6455316400bff7
BernhardBaumrock <office@baumrock.com>
2022-03-28 18:19:30 +0200

    update stubs

--- stubs/layouts/default.php
+++ stubs/layouts/default.php
@@ -1,6 +1,9 @@
-<?php namespace ProcessWire;
-echo $rf->render("sections/header");
-echo "<main>";
-echo $rf->render("sections/main");
-echo "</main>";
-echo $rf->render("sections/footer");
+<div class='tm-grid-cols'>
+  <div></div>
+  <div class='tm-grid-rows uk-card uk-card-default'>
+    <?= $rf->render("sections/header") ?>
+    <main><?= $rf->render("sections/main") ?></main>
+    <?= $rf->render("sections/footer") ?>
+  <div>
+  <div></div>
+</div>
