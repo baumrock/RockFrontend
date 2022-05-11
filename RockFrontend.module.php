816e97f19adb22614ff7fe56a74c4294af35bbbb
BernhardBaumrock <office@baumrock.com>
2022-05-11 09:05:12 +0200

    add editlinks

--- RockFrontend.module.php
+++ RockFrontend.module.php
@@ -44,7 +44,7 @@ class RockFrontend extends WireData implements Module {
   public static function getModuleInfo() {
     return [
       'title' => 'RockFrontend',
-      'version' => '1.3.0',
+      'version' => '1.3.1',
       'summary' => 'Module for easy frontend development',
       'autoload' => true,
       'singular' => true,
@@ -255,6 +255,23 @@ class RockFrontend extends WireData implements Module {
       'addBottom' => $opt->addBottom,
     ]);
     return " alfred='$str'";
+  }
+
+  public function editLinks($page = null) {
+    if(!$page) $page = $this->wire->page;
+    $pages = $this->wire->pages;
+    return "
+      <li>
+        <a href='{$pages->get(2)->url}'>
+          <svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' aria-hidden='true' role='img' class='iconify iconify--tabler' width='32' height='32' preserveAspectRatio='xMidYMid meet' viewBox='0 0 24 24'><g fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'><rect width='6' height='6' x='3' y='15' rx='2'></rect><rect width='6' height='6' x='15' y='15' rx='2'></rect><rect width='6' height='6' x='9' y='3' rx='2'></rect><path d='M6 15v-1a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v1m-6-6v3'></path></g></svg>
+        </a>
+      </li>
+      <li>
+        <a href='{$page->editUrl()}'>
+          <svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' aria-hidden='true' role='img' class='iconify iconify--tabler' width='32' height='32' preserveAspectRatio='xMidYMid meet' viewBox='0 0 24 24'><g fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'><path d='M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1'></path><path d='M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3l8.385-8.415zM16 5l3 3'></path></g></svg>
+        </a>
+      </li>
+    ";
   }
 
   /**
