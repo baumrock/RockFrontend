c34beb39c1c07e592e664aab6f61180bb828ef8b
BernhardBaumrock <office@baumrock.com>
2022-06-28 10:02:55 +0200

    add latte source to module

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9895717183817f9b5c96da709a6abad981206a93',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9895717183817f9b5c96da709a6abad981206a93',
+            'dev_requirement' => false,
+        ),
+        'latte/latte' => array(
+            'pretty_version' => 'v3.0.2',
+            'version' => '3.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../latte/latte',
+            'aliases' => array(),
+            'reference' => 'c3eee2e4e2c21cdf9f9c158c4bfa6150625457e1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
