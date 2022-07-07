d2bbbb577e7e88a752c979484a2db76e9c2f841b
BernhardBaumrock <office@baumrock.com>
2022-07-07 16:55:31 +0200

    improve uikit frontend markup

--- profiles/uikit/files/site/templates/layouts/default.php
+++ profiles/uikit/files/site/templates/layouts/default.php
@@ -1,9 +1,4 @@
-<div class='tm-grid-cols uk-background-muted'>
-  <div></div>
-  <div class='tm-grid-rows uk-card uk-card-default'>
-    <?= $rf->render("sections/header") ?>
-    <main><?= $rf->render("sections/main") ?></main>
-    <?= $rf->render("sections/footer") ?>
-  <div>
-  <div></div>
-</div>
+<?php
+echo $rf->render("sections/header");
+echo $rf->render("sections/main");
+echo $rf->render("sections/footer");
