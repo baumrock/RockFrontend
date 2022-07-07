a55cb2770b572c5cfb4eb5ff675963b802242ccb
BernhardBaumrock <office@baumrock.com>
2022-07-07 17:09:20 +0200

    update uikit profile

--- profiles/uikit/files/site/templates/layouts/default.php
+++ profiles/uikit/files/site/templates/layouts/default.php
@@ -1,4 +1,7 @@
-<?php
-echo $rf->render("sections/header");
-echo $rf->render("sections/main");
-echo $rf->render("sections/footer");
+<div class="tmg-boxed tmg-main-grow">
+  <?php
+  echo $rf->render("sections/header");
+  echo $rf->render("sections/main");
+  echo $rf->render("sections/footer");
+  ?>
+</div>
