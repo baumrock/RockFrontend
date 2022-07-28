692db830c6637686211487e85c38cde604d6a9a1
BernhardBaumrock <office@baumrock.com>
2022-07-28 16:07:34 +0200

    bump version

--- RockFrontend.module.php
+++ RockFrontend.module.php
@@ -49,7 +49,7 @@ class RockFrontend extends WireData implements Module, ConfigurableModule {
   public static function getModuleInfo() {
     return [
       'title' => 'RockFrontend',
-      'version' => '1.10.1',
+      'version' => '1.10.2',
       'summary' => 'Module for easy frontend development',
       'autoload' => true,
       'singular' => true,
