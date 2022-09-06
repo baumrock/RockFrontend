8da7c88aa7754f16d215a65e6103f2cfe75408c5
BernhardBaumrock <office@baumrock.com>
2022-09-06 15:41:43 +0200

    fix vscode links closing watchstream

--- livereload.js
+++ livereload.js
@@ -4,35 +4,47 @@
 setTimeout(() => {
   let isModal = !!document.querySelector('body.modal');
   if(isModal) return;
-
-  console.log('RockFrontend is listening for file changes...');
   let reloading = false;
-  const evtSource = new EventSource(
-    '/livereload.php?secret='+rf_livereload_secret,
-    { withCredentials: true }
-  );
+
+  let evtSource;
+  let startStream = function() {
+    evtSource = new EventSource(
+      '/livereload.php?secret='+rf_livereload_secret,
+      { withCredentials: true }
+    );
+    evtSource.onmessage = function(event) {
+      let changes = JSON.parse(event.data);
+      if(changes.length && !reloading) {
+        console.log(changes);
+        // check if we are in the admin and have unsaved changes
+        if(document.querySelectorAll('.InputfieldStateChanged').length) {
+          console.log('detected change - unsaved changes prevent reload');
+          return;
+        }
+        if(document.querySelectorAll('#pw-panel-shade').length) {
+          console.log('detected change - open panel prevents reload');
+          return;
+        }
+        // all fine, reload page
+        reloading = true;
+        console.log('detected change - reloading', changes);
+        document.location.reload(true);
+      }
+      else if(!reloading) console.log('no change');
+    }
+  }
+
+  startStream();
+  console.log('RockFrontend is listening for file changes...');
+
   // prevent error msg in Firefox https://bugzilla.mozilla.org/show_bug.cgi?id=833462
   window.addEventListener('beforeunload', () => {
     evtSource.close();
+    setTimeout(() => {
+      // we restart the stream because clicks on vscode links would
+      // otherwise stop the livereload watcher stream!
+      startStream();
+    }, 50);
   });
-  evtSource.onmessage = function(event) {
-    let changes = JSON.parse(event.data);
-    if(changes.length && !reloading) {
-      console.log(changes);
-      // check if we are in the admin and have unsaved changes
-      if(document.querySelectorAll('.InputfieldStateChanged').length) {
-        console.log('detected change - unsaved changes prevent reload');
-        return;
-      }
-      if(document.querySelectorAll('#pw-panel-shade').length) {
-        console.log('detected change - open panel prevents reload');
-        return;
-      }
-      // all fine, reload page
-      reloading = true;
-      console.log('detected change - reloading', changes);
-      document.location.reload(true);
-    }
-    else if(!reloading) console.log('no change');
-  }
+
 }, 1000);
