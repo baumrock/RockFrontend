e5a96b554360a3b137b5e7108963fe0b37146d6c
BernhardBaumrock <office@baumrock.com>
2022-09-27 13:56:25 +0200

    refactor

--- uikit/GridDropdown.js
+++ uikit/GridDropdown.js
@@ -5,7 +5,7 @@
  * v1.1
  */
 
-(function(){
+(function () {
   let util = UIkit.util;
   let id = 0;
 
@@ -19,19 +19,19 @@
   /**
    * Close all given items
    */
-  GridDropdowns.prototype.closeAll = function(items, callback) {
-    util.each(items, function(item) {
-      if(typeof item == 'undefined') return;
-      if(!item.isOpen) return;
+  GridDropdowns.prototype.closeAll = function (items, callback) {
+    util.each(items, function (item) {
+      if (typeof item == "undefined") return;
+      if (!item.isOpen) return;
       item.close();
     });
     this.whenClosed(items, callback);
-  }
+  };
 
   /**
    * Reload all open dropdowns (when window is resized)
    */
-  GridDropdowns.prototype.reloadAll = function() {
+  GridDropdowns.prototype.reloadAll = function () {
     let openItems = this.getOpen(this.dropdowns);
     let afterClose = () => {
       // let last;
@@ -40,71 +40,70 @@
         // last = item;
       });
       // util.scrollIntoView(last);
-    }
+    };
     this.closeAll(this.dropdowns, afterClose);
-  }
+  };
 
   /**
    * Get dropdown by id
    */
-  GridDropdowns.prototype.get = function(id) {
+  GridDropdowns.prototype.get = function (id) {
     return this.dropdowns[id];
-  }
+  };
 
   /**
    * Get dropdown are init a new one
    */
-  GridDropdowns.prototype.getDropdown = function(el) {
+  GridDropdowns.prototype.getDropdown = function (el) {
     let dropdown = false;
 
     // option 1: a toggle button was clicked
-    let toggle = el.closest('.rf-griddropdown-toggle');
-    if(toggle) {
-      let id = util.data(toggle, 'griddropdown-id');
-      if(!id) {
+    let toggle = el.closest(".rf-griddropdown-toggle");
+    if (toggle) {
+      let id = util.data(toggle, "griddropdown-id");
+      if (!id) {
         // create a new dropdown
         dropdown = new GridDropdown(toggle);
         this.dropdowns[dropdown.id] = dropdown;
-      }
-      else dropdown = this.dropdowns[id];
+      } else dropdown = this.dropdowns[id];
       return dropdown;
     }
 
     // option 2: an element was provided to return the dropdown
     // in this case we dont create a new dropdown if we dont find one
-    el = el.closest('[data-griddropdown-id]');
-    let id = util.data(el, 'griddropdown-id');
+    el = el.closest("[data-griddropdown-id]");
+    let id = util.data(el, "griddropdown-id");
     return dropdowns.get(id);
-  }
+  };
 
   /**
    * Get all open items
    */
-  GridDropdowns.prototype.getOpen = function(items) {
+  GridDropdowns.prototype.getOpen = function (items) {
     let open = [];
     util.each(items, (item) => {
-      if(item.isOpen) open.push(item);
+      if (item.isOpen) open.push(item);
     });
     return open;
-  }
+  };
 
   /**
    * Helper that executes a callback when all items have been closed
    */
-  GridDropdowns.prototype.whenClosed = function(items, callback) {
+  GridDropdowns.prototype.whenClosed = function (items, callback) {
     // console.log('check all closed?', items);
     let loop = setInterval(() => {
       // console.log('checking');
       let open = false;
       util.each(items, (item) => {
-        if(item.isOpen) open = true;
+        if (item.isOpen) open = true;
       });
-      if(!open) {
+      if (!open) {
         clearInterval(loop);
         callback();
       }
     }, 50);
-  }
+  };
 
   // ########## GridDropdown ##########
 
@@ -121,157 +120,165 @@
     this.isOpen = false;
 
     this.findGridItem(toggle);
-    util.attr(this.gridItem, 'data-griddropdown-id', this.id);
-    util.attr(toggle, 'data-griddropdown-id', this.id);
+    util.attr(this.gridItem, "data-griddropdown-id", this.id);
+    util.attr(toggle, "data-griddropdown-id", this.id);
   }
 
   /**
    * Close the info popup
    */
-  GridDropdown.prototype.close = function() {
-    if(!this.isOpen) return;
+  GridDropdown.prototype.close = function () {
+    if (!this.isOpen) return;
 
     // hide close icon (fix layout shift)
-    let close = util.$$('[uk-close]', this.getAcc());
-    util.addClass(close, 'uk-hidden');
+    let close = util.$$("[uk-close]", this.getAcc());
+    util.addClass(close, "uk-hidden");
 
     // toggle accordion
     this.getAcc(true).toggle();
-  }
+  };
 
   /**
    * Create the div to place the popup
    */
-  GridDropdown.prototype.createAccordion = function() {
+  GridDropdown.prototype.createAccordion = function () {
     let items = this.rowItems();
     let last = items.slice(-1)[0];
-    util.after(last,
-      "<div class='rf-griddropdown-container uk-width-1-1 uk-margin-remove' data-griddropdown-id='"+this.id+"'>"
-        +'<ul class="uk-margin-remove" uk-accordion>'
-          +'<li>'
-            +'<div class="uk-accordion-content uk-position-relative rf-griddropdown-inner"></div>'
-          +'</li>'
-        +'</ul>'
-      +"</div>"
+    util.after(
+      last,
+      "<div class='rf-griddropdown-container uk-width-1-1 uk-margin-remove' data-griddropdown-id='" +
+        this.id +
+        "'>" +
+        '<ul class="uk-margin-remove" uk-accordion>' +
+        "<li>" +
+        '<div class="uk-accordion-content uk-position-relative rf-griddropdown-inner"></div>' +
+        "</li>" +
+        "</ul>" +
+        "</div>"
     );
 
     let info = this.getInfo(true);
     util.html(info, this.getMarkup());
 
     // fix close button layout shift issue
-    util.addClass(util.$$('[uk-close]', info), 'uk-hidden');
+    util.addClass(util.$$("[uk-close]", info), "uk-hidden");
 
     return this.getAcc(true);
-  }
+  };
 
   /**
    * Populate the gridItem property of this object
    */
-  GridDropdown.prototype.findGridItem = function(el) {
-    while(el.parentNode) {
+  GridDropdown.prototype.findGridItem = function (el) {
+    while (el.parentNode) {
       this.gridItem = el;
       el = el.parentNode;
-      if(util.hasClass(el, 'rf-griddropdown')) return;
+      if (util.hasClass(el, "rf-griddropdown")) return;
     }
-  }
+  };
 
   /**
    * Get accordion element
    */
-  GridDropdown.prototype.getAcc = function(getComponent) {
-    let acc = util.$('[uk-accordion]', this.getInfo());
-    if(getComponent) return UIkit.accordion(acc);
+  GridDropdown.prototype.getAcc = function (getComponent) {
+    let acc = util.$("[uk-accordion]", this.getInfo());
+    if (getComponent) return UIkit.accordion(acc);
     return acc;
-  }
+  };
 
   /**
    * Get accordion elements container
    */
-  GridDropdown.prototype.getAccContainer = function() {
-    return this.getAcc().closest('.rf-griddropdown-container');
-  }
+  GridDropdown.prototype.getAccContainer = function () {
+    return this.getAcc().closest(".rf-griddropdown-container");
+  };
 
   /**
    * Get info dropdown element (or inner element)
    */
-  GridDropdown.prototype.getInfo = function(inner) {
-    inner = inner ? ' .rf-griddropdown-inner' : '';
-    return util.$('.rf-griddropdown-container[data-griddropdown-id="'+this.id+'"] '+inner);
-  }
+  GridDropdown.prototype.getInfo = function (inner) {
+    inner = inner ? " .rf-griddropdown-inner" : "";
+    return util.$(
+      '.rf-griddropdown-container[data-griddropdown-id="' +
+        this.id +
+        '"] ' +
+        inner
+    );
+  };
 
   /**
    * Get the markup of the info dropdown
    */
-  GridDropdown.prototype.getMarkup = function() {
+  GridDropdown.prototype.getMarkup = function () {
     return this.markup.innerHTML;
-  }
+  };
 
   /**
    * Return the grid element of the current popup
    */
-  GridDropdown.prototype.grid = function() {
-    return this.toggle.closest('.rf-griddropdown');
-  }
+  GridDropdown.prototype.grid = function () {
+    return this.toggle.closest(".rf-griddropdown");
+  };
 
   /**
    * Get all griditems
    */
-  GridDropdown.prototype.getItems = function() {
-    if(this.gridItems) return this.gridItems;
+  GridDropdown.prototype.getItems = function () {
+    if (this.gridItems) return this.gridItems;
     let items = [];
-    util.each(util.$$('>*', this.grid()), function(griditem) {
-      if(util.hasClass(griditem, 'rf-griddropdown-container')) return;
+    util.each(util.$$(">*", this.grid()), function (griditem) {
+      if (util.hasClass(griditem, "rf-griddropdown-container")) return;
       items.push(griditem);
     });
-    return this.gridItems = items;
-  }
+    return (this.gridItems = items);
+  };
 
   /**
    * Return all items of the row the item lives in (currently)
    * Note that this can change on every click!
    */
-  GridDropdown.prototype.rowItems = function(returnObjects) {
+  GridDropdown.prototype.rowItems = function (returnObjects) {
     let rowitems = [];
     let dropdown = this;
     let top = this.top();
     // console.log(top, 'top');
-    util.each(this.getItems(), function(item) {
+    util.each(this.getItems(), function (item) {
       let ctop = dropdown.top(item);
-      if(ctop != top) return;
-      if(returnObjects) item = dropdowns.getDropdown(item);
-      if(item) rowitems.push(item);
+      if (ctop != top) return;
+      if (returnObjects) item = dropdowns.getDropdown(item);
+      if (item) rowitems.push(item);
     });
     return rowitems;
-  }
+  };
 
   /**
    * Set width of arrow container to match the grid item
    */
-  GridDropdown.prototype.setArrowWidth = function() {
+  GridDropdown.prototype.setArrowWidth = function () {
     let item = this.gridItem;
     let acc = this.getAcc();
     let width = util.width(item);
     let itemleft = item.getBoundingClientRect().left;
 
     // get reference to compare the left offset with
-    let ref = acc.closest('.rf-griddropdown-container');
+    let ref = acc.closest(".rf-griddropdown-container");
     let refleft = ref.getBoundingClientRect().left;
     // console.log(itemleft, item);
     // console.log(refleft, ref);
 
     // find arrow
-    let arrow = util.$('.rf-griddropdown-arrow', acc);
-    util.addClass(arrow, 'uk-position-absolute uk-flex uk-flex-center');
+    let arrow = util.$(".rf-griddropdown-arrow", acc);
+    util.addClass(arrow, "uk-position-absolute uk-flex uk-flex-center");
     util.width(arrow, width);
-    arrow.style.left = Math.floor(itemleft-refleft)+"px";
-  }
+    arrow.style.left = Math.floor(itemleft - refleft) + "px";
+  };
 
   /**
    * Show the popup
    */
-  GridDropdown.prototype.show = function() {
-    if(this.isOpen) return;
-    if(this.showing) return; // prevents doubleclick issues
+  GridDropdown.prototype.show = function () {
+    if (this.isOpen) return;
+    if (this.showing) return; // prevents doubleclick issues
     let dropdown = this;
     this.showing = true;
     // we fetch acc outside of the callback to make sure
@@ -285,15 +292,15 @@
       dropdown.setArrowWidth();
       acc.toggle();
     });
-  }
+  };
 
   /**
    * Get top offset of element
    */
-  GridDropdown.prototype.top = function(el) {
+  GridDropdown.prototype.top = function (el) {
     el = el || this.gridItem;
-    return el.getBoundingClientRect().top
-  }
+    return el.getBoundingClientRect().top;
+  };
 
   // ########## init ##########
 
@@ -301,50 +308,49 @@
   let dropdowns = new GridDropdowns();
 
   // handle clicks on toggle
-  util.on('.rf-griddropdown-toggle', 'click', function(e) {
+  util.on(".rf-griddropdown-toggle", "click", function (e) {
     e.preventDefault();
     let dropdown = dropdowns.getDropdown(e.target);
     dropdown.show();
   });
 
   // listen to toggle closed events
-  util.on(document, 'hidden', function(e) {
+  util.on(document, "hidden", function (e) {
     let el = e.target;
     let dropdown = dropdowns.getDropdown(el);
-    if(!dropdown) return;
-    util.removeClass(dropdown.gridItem, 'rf-griddropdown-open');
+    if (!dropdown) return;
+    util.removeClass(dropdown.gridItem, "rf-griddropdown-open");
     dropdown.isOpen = false;
     util.remove(dropdown.getAccContainer());
   });
 
   // listen to toggle opened events
-  util.on(document, 'shown', function(e) {
+  util.on(document, "shown", function (e) {
     let el = e.target;
-    if(!util.$('>div.rf-griddropdown-inner', el)) return;
+    if (!util.$(">div.rf-griddropdown-inner", el)) return;
     let dropdown = dropdowns.getDropdown(el);
-    util.addClass(dropdown.gridItem, 'rf-griddropdown-open');
+    util.addClass(dropdown.gridItem, "rf-griddropdown-open");
 
     // show close button
-    let close = util.$$('[uk-close]', dropdown.getAcc());
-    util.removeClass(close, 'uk-hidden');
+    let close = util.$$("[uk-close]", dropdown.getAcc());
+    util.removeClass(close, "uk-hidden");
   });
 
   // auto-close open dropdowns
-  util.on(document, 'click', function(e) {
-    let el = e.target.closest('[uk-close]');
-    if(!el) return;
+  util.on(document, "click", function (e) {
+    let el = e.target.closest("[uk-close]");
+    if (!el) return;
     let dropdown = dropdowns.getDropdown(el);
-    if(!dropdown) return;
+    if (!dropdown) return;
     dropdown.close();
   });
 
   // reload all dropdowns on resize
   let timer;
-  util.on(window, 'resize', function() {
+  util.on(window, "resize", function () {
     clearTimeout(timer);
     timer = setTimeout(() => {
       dropdowns.reloadAll();
     }, 250);
   });
-
 })();
