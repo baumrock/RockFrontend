70c34cddfdf45f641e86c9a65fbf856d4aa5cd6b
BernhardBaumrock <office@baumrock.com>
2022-11-07 15:32:00 +0100

    add 12-column-grid

--- RockFrontend.module.php
+++ RockFrontend.module.php
@@ -102,7 +102,7 @@ class RockFrontend extends WireData implements Module, ConfigurableModule
   {
     return [
       'title' => 'RockFrontend',
-      'version' => '2.6.2',
+      'version' => '2.6.3',
       'summary' => 'Module for easy frontend development',
       'autoload' => true,
       'singular' => true,
