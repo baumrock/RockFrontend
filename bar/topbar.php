7f56583abe10a3d2ac011d6f57c58565b1bb4781
BernhardBaumrock <office@baumrock.com>
2023-03-01 15:09:26 +0100

    feat: add hide icon to topbar and rename rpb to rf

--- bar/topbar.php
+++ bar/topbar.php
@@ -1,6 +1,6 @@
-<div id="rpb-topbar" style="z-index: <?= $z ?>">
+<div id="rf-topbar" style="z-index: <?= $z ?>">
   <a href="https://www.baumrock.com" target=_blank>
-    <img id="rpb-logo" src="<?= $logourl ?>">
+    <img id="rf-logo" src="<?= $logourl ?>">
   </a>
 
   <a href="<?= $pages->get(2)->url ?>">
@@ -22,6 +22,14 @@
       </g>
     </svg>
   </a>
+  <a href="#" class="rf-topbar-hide">
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+      <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+        <path d="m3 3l18 18M10.584 10.587a2 2 0 0 0 2.828 2.83" />
+        <path d="M9.363 5.365A9.466 9.466 0 0 1 12 5c4 0 7.333 2.333 10 7c-.778 1.361-1.612 2.524-2.503 3.488m-2.14 1.861C15.726 18.449 13.942 19 12 19c-4 0-7.333-2.333-10-7c1.369-2.395 2.913-4.175 4.632-5.341" />
+      </g>
+    </svg>
+  </a>
 
   <?php if ($user->isSuperuser()) : ?>
     <a href="<?= $pages->get(2)->url . "setup/template/edit?id=" . $page->template->id ?>">
@@ -35,7 +43,7 @@
     </a>
   <?php endif; ?>
 
-  <a href=/ class="rpb-device-preview">
+  <a href=/ class="rf-device-preview">
     <svg width="100%" height="100%" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;">
       <g>
         <g transform="matrix(1,0,0,1,-1,0)">
@@ -57,18 +65,18 @@
   </a>
 
 </div>
-<div id="rpb-device-preview">
+<div id="rf-device-preview">
   <div class="iframe-wrapper">
     <div></div>
-    <div class="rpb-devices">
-      <a href=/ class="rpb-close">
+    <div class="rf-devices">
+      <a href=/ class="rf-close">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
           <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 6L6 18M6 6l12 12" />
         </svg>
       </a>
     </div>
     <div>
-      <iframe name="rpb-preview" data-src="<?= $this->wire->input->url ?>">
+      <iframe name="rf-preview" data-src="<?= $this->wire->input->url ?>">
       </iframe>
     </div>
     <div></div>
@@ -76,30 +84,34 @@
 </div>
 <script>
   (function() {
-    let $bar = document.querySelector("#rpb-topbar");
-    let $preview = document.querySelector("#rpb-device-preview");
+    let $bar = document.querySelector("#rf-topbar");
+    let $preview = document.querySelector("#rf-device-preview");
 
     // we are in the rockfrontend preview iframe
-    if (window.name === 'rpb-preview') {
-      document.querySelector('body').classList.add('rpb-preview');
+    if (window.name === 'rf-preview') {
+      document.querySelector('body').classList.add('rf-preview');
     }
 
     // listen to clicks
     document.addEventListener('click', function(event) {
       let $a = event.target.closest('a');
       if (!$a) return;
-      if ($a.matches('.rpb-device-preview')) {
+      if ($a.matches('.rf-device-preview')) {
         // click on devices icon in bar
         event.preventDefault();
         $preview.classList.toggle('show');
         document.querySelector('body').classList.toggle('overflow-hidden');
         let $iframe = $preview.querySelector('iframe');
         $iframe.setAttribute('src', $iframe.dataset.src);
-      } else if ($a.matches('.rpb-close')) {
+      } else if ($a.matches('.rf-close')) {
         // close preview modal
         event.preventDefault();
         $preview.classList.remove('show');
         document.querySelector('body').classList.remove('overflow-hidden');
+      } else if ($a.matches('.rf-topbar-hide')) {
+        // close preview modal
+        event.preventDefault();
+        $a.closest('#rf-topbar').remove();
       }
     }, false);
   })()
