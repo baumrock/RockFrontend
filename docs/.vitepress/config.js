6500881d9be49602603066c9d87381323f1b7780
BernhardBaumrock <office@baumrock.com>
2023-03-11 17:24:19 +0100

    feat: add vitepress docs

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,11 @@
+// .vitepress/config.js
+export default {
+  // site-level options
+  title: "RockFrontend Docs",
+  description: "Toolbox for Frontend Development with ProcessWire",
+  base: "/docs/rockfrontend/",
+  themeConfig: {
+    // logo does not work on build!
+    // logo: "logo.svg",
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,4 @@
+
+# RockFrontend 🚀🚀 The Powerful Toolbox for ProcessWire Frontend Development
+
+Awesome Docs for RockFrontend - coming soon!
