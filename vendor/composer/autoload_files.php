6c2dd06c1ef43e4e471fdc183b86055347ad9f56
BernhardBaumrock <office@baumrock.com>
2023-07-04 17:31:50 +0200

    fix: update composer dependencies

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -8,5 +8,4 @@ $baseDir = dirname($vendorDir);
 return array(
     '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
     '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
-    'f91de9311f7b320431569e8469ba6c76' => $vendorDir . '/baumrock/humandates/HumanDates.php',
 );
