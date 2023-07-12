e92f9bc88334af6fb3b8c0457b747215306fef94
BernhardBaumrock <office@baumrock.com>
2023-07-12 16:38:15 +0200

    fix: remove outdated uikit profile - please use the rock profile instead!

--- profiles/uikit/files/site/templates/layouts/default.php
+++
@@ -1,7 +0,0 @@
-<div class="tmg-box-outer tmg-main-grow">
-  <?php
-  echo $rf->render("sections/header");
-  echo $rf->render("sections/main");
-  echo $rf->render("sections/footer");
-  ?>
-</div>
