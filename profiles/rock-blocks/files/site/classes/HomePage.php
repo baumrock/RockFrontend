27ded7afb32be088c1b934517808945f8227cf8e
BernhardBaumrock <office@baumrock.com>
2023-10-13 15:07:59 +0200

    feat: add rockblocks profile

---
+++ profiles/rock-blocks/files/site/classes/HomePage.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ProcessWire;
+
+use RockMigrations\MagicPage;
+
+/**
+ * This is a custom page class for the HomePage
+ * You can delete this file if you don't need it.
+ */
+class HomePage extends Page
+{
+  use MagicPage;
+
+  const tpl = 'home';
+
+  /** magic */
+
+  public function editForm($form)
+  {
+    $rm = $this->rockmigrations();
+
+    // global site seo wrapper
+    // add and remove fields as you like
+    $rm->wrapFields($form, [
+      RockFrontend::field_favicon,
+      RockFrontend::field_ogimage,
+    ], [
+      'label' => 'Global Site SEO Settings',
+      'collapsed' => Inputfield::collapsedYes,
+    ]);
+
+    // global site settings
+    // add and remove fields as you like
+    $rm->wrapFields($form, [
+      RockFrontend::field_footerlinks,
+    ], [
+      'label' => 'Global Site Settings',
+      'collapsed' => Inputfield::collapsedYes,
+    ]);
+  }
+
+  public function migrate()
+  {
+    $rm = $this->rockmigrations();
+    $rm->installModule("RockPageBuilder");
+    $rm->migrate([
+      'fields' => [],
+      'templates' => [
+        self::tpl => [
+          'fields' => [
+            'title',
+            // RockPageBuilder::field_blocks,
+            // RockPageBuilder::field_widgets,
+
+            RockFrontend::field_favicon,
+            RockFrontend::field_ogimage,
+            RockFrontend::field_footerlinks,
+          ],
+        ],
+      ],
+    ]);
+  }
+}
---
+++ profiles/rock-blocks/files/site/templates/layouts/default.php
@@ -0,0 +1,4 @@
+<?php
+echo $rf->render("sections/header");
+echo $rf->render("sections/main");
+echo $rf->render("sections/footer");
