15ad6cfa032c0b37c6e6db10d608f8d80d975b2e
BernhardBaumrock <office@baumrock.com>
2024-01-28 11:42:54 +0100

    feat: update profile to use layout.latte

---
+++ profiles/rockpagebuilder/files/site/templates/_init.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ProcessWire;
+
+$htmlLang = "de";
+
+$rockfrontend->styles()
+  ->add('/site/templates/uikit/src/less/uikit.theme.less')
+  ->add('/site/modules/RockFrontend/less/defaults.less')
+  ->addDefaultFolders()
+  ->minify(!$config->debug);
+
+$rockfrontend->scripts()
+  ->add('/site/templates/uikit/dist/js/uikit.min.js')
+  ->add('/site/templates/scripts/main.js', 'defer')
+  ->minify(!$config->debug);
+
+$seo = $rockfrontend->seo()
+  ->title($page->title . " | example.com");
