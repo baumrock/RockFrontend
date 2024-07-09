d43f14fcf4644750da90331ffd78164352ddc870
BernhardBaumrock <office@baumrock.com>
2024-07-09 21:56:18 +0200

    fix: remove reload on error

--- livereload.js
+++ livereload.js
@@ -85,10 +85,7 @@ setTimeout(() => {
 
     evtSource.onerror = function (event) {
       if (document.querySelector("#tracy-bs")) return;
-      console.log("Error occurred in EventSource, reloading window.");
-      setTimeout(() => {
-        window.location.reload();
-      }, 1500);
+      console.log("Error occurred in EventSource.");
     };
   };
 
