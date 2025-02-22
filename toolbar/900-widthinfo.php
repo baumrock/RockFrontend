e413625238c563295af1dbd765a9263d6ff4316c
BernhardBaumrock <office@baumrock.com>
2025-02-22 12:30:06 +0100

    feat: add tailwind width info to toolbar

---
+++ toolbar/900-widthinfo.php
@@ -0,0 +1,31 @@
+<?php if (!$user->isSuperuser()) return ?>
+<style>
+  #toolbar-widthinfo .breakpoints {
+    display: inline-flex;
+  }
+
+  #toolbar-widthinfo .breakpoints>* {
+    display: none;
+  }
+</style>
+<span id='toolbar-widthinfo' class='flex gap-2'>
+  <small></small>
+  <span class='breakpoints'>
+    <small class='font-bold xs:!inline-flex sm:!hidden'>xs</small>
+    <small class='font-bold sm:!inline-flex md:!hidden'>sm</small>
+    <small class='font-bold md:!inline-flex lg:!hidden'>md</small>
+    <small class='font-bold lg:!inline-flex xl:!hidden'>lg</small>
+    <small class='font-bold xl:!inline-flex'>xl</small>
+  </span>
+</span>
+<script>
+  // write current width to <small> in #toolbar-widthinfo
+  document.addEventListener('DOMContentLoaded', function() {
+    const updateWidth = () => {
+      const el = document.querySelector('#toolbar-widthinfo small');
+      el.textContent = window.innerWidth + "px";
+    };
+    window.addEventListener('resize', updateWidth);
+    updateWidth();
+  });
+</script>
