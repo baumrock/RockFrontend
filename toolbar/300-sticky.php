7a4a2349ae57bc3c0aad89c6b141006d80608dfd
BernhardBaumrock <office@baumrock.com>
2025-07-20 16:58:59 +0200

    fix: toolbar alfred issue

--- toolbar/300-sticky.php
+++ toolbar/300-sticky.php
@@ -2,6 +2,7 @@
   title="<?= $wire->_('Sticky on/off') ?>"
   uk-tooltip
   data-toggle="sticky"
+  data-instant
   data-persist>
   <svg class='visible-on' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
     <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 3a2 2 0 0 1 2 2m0 4v4a3 3 0 0 0 5.552 1.578M15 11V5a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v8a7.99 7.99 0 0 1-.424 2.577m-1.463 2.584A8 8 0 0 1 4 13V5c0-.297.065-.58.181-.833M4 8h4m7 0h4M3 3l18 18" />
